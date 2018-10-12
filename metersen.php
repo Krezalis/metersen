da489ec73aacbcca2ea4e4c6b47e971b61883702
Krezalis <krezalis@gmail.com>
2018-10-12 19:09:52 +0300

    Update metersen.php
    Додано внечення через EnergySuite.Online по особовому рахунку, додано дебаг режим кожного кроку. Ревакторинг коду.

--- metersen.php
+++ metersen.php
@@ -41,7 +41,18 @@ function loe_webchat($persid,$count){
   };
 };
 
-function EnergySuite($ConsumerId,$CurrentValue,$Email,$Password,$domain='info.loe.lviv.ua'){
+function txt_p($post){
+ $post_txt='';
+ foreach ($post as $k=>$v) {$post_txt.=(empty($post_txt)?'':'&').urlencode($k).'='.urlencode($v);}; 
+ return $post_txt;  
+}; 
+function txt_c($cookie){
+  $cookie_txt='';
+  foreach ($cookie as $k=>$v) {$cookie_txt.=urlencode($k).'='.urlencode($v).'; ';};
+  return $cookie_txt;
+};  
+
+function EnergySuite($ConsumerId,$CurrentValue,$Email,$Password,$domain='info.loe.lviv.ua',$debug=false){
   //send meters to EnergySuite.Online by https://www.extracode.com.ua/
   /* 
      https://info.loe.lviv.ua/ - Львівобленерго
@@ -50,142 +61,179 @@ function EnergySuite($ConsumerId,$CurrentValue,$Email,$Password,$domain='info.lo
      https://my.toe.com.ua/ - ВАТ Тернопільобленерго 
   */ 
   
-  $S=array(
-    'ASP.NET_SessionId'=>'',
-    '__RequestVerificationToken-HEADER'=>'',
-    'ARRAffinity'=>'',
-    '__RequestVerificationToken-FORM'=>'',
-    '.AspNet.ApplicationCookie'=>'',
-    '.AspNet.TwoFactorCookie'=>'',
-    '.AspNet.ExternalCookie'=>'',
-    'Consumer'=>array()
-  );
+  $success=true;
+  $log=array();
+  
+  $Consumer=array();
+  $FRVT='';
+  $cookie=array();
+  
+  $log[]='Використано домен: '.$domain;
   
   //1. отримати форму авторизації і CRF токен
   $ch = curl_init('https://'.$domain.'/Account/Login');
   curl_setopt($ch, CURLOPT_HEADER, 1);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
   $result = curl_exec($ch); 
-  $info=curl_getinfo($ch);
   curl_close($ch);
-  preg_match('/Set-Cookie: ASP.NET_SessionId=([a-z0-9]*); path=\/; HttpOnly/', $result, $matches, PREG_OFFSET_CAPTURE);
-  if (isset($matches[1][0])){$S['ASP.NET_SessionId']=$matches[1][0];};
-  preg_match('/Set-Cookie: ARRAffinity=([a-z0-9]*);/', $result, $matches, PREG_OFFSET_CAPTURE);
-  if (isset($matches[1][0])){$S['ARRAffinity']=$matches[1][0];};
-  preg_match('/Set-Cookie: __RequestVerificationToken=([a-zA-Z0-9_-]*); path=\/; HttpOnly/', $result, $matches, PREG_OFFSET_CAPTURE);
-  if (isset($matches[1][0])){$S['__RequestVerificationToken-HEADER']=$matches[1][0];};
-  preg_match('/__RequestVerificationToken" type="hidden" value="([a-zA-Z0-9_-]*)" \/>/', $result, $matches, PREG_OFFSET_CAPTURE);
-  if (isset($matches[1][0])){$S['__RequestVerificationToken-FORM']=$matches[1][0];}; 
-  
+  preg_match('/Set-Cookie: ASP.NET_SessionId=([a-z0-9]*); path=\/; HttpOnly/', $result, $m, PREG_OFFSET_CAPTURE);
+  if (isset($m[1][0])){$cookie['ASP.NET_SessionId']=$m[1][0];}else{$success=false; $log[]='Не знайдено ASP.NET_SessionId';};
+  preg_match('/Set-Cookie: ARRAffinity=([a-z0-9]*);/', $result, $m, PREG_OFFSET_CAPTURE);
+  if (isset($m[1][0])){$cookie['ARRAffinity']=$m[1][0];}else{$success=false; $log[]='Не знайдено Cookie ARRAffinity';};
+  preg_match('/Set-Cookie: __RequestVerificationToken=([a-zA-Z0-9_-]*); path=\/; HttpOnly/', $result, $m, PREG_OFFSET_CAPTURE);
+  if (isset($m[1][0])){$cookie['__RequestVerificationToken']=$m[1][0];}else{$success=false; $log[]='Не знайдено Cookie __RequestVerificationToken';};
+  preg_match('/__RequestVerificationToken" type="hidden" value="([a-zA-Z0-9_-]*)" \/>/', $result, $m, PREG_OFFSET_CAPTURE);
+  if (isset($m[1][0])){$FRVT=$m[1][0];}else{$success=false; $log[]='Не знайдено у формі __RequestVerificationToken';}; 
+    
   //2. авторизуватися і отримати токен сесії
-  $post=array(
-    '__RequestVerificationToken'=>$S['__RequestVerificationToken-FORM'],
-    'Email'=>$Email,
-    'Password'=>$Password,
-    'RememberMe'=>'false'
-  );
-  $post_txt='';foreach ($post as $k=>$v) {$post_txt.=(empty($post_txt)?'':'&').urlencode($k).'='.urlencode($v);};
-  $cookie=array(
-    '__RequestVerificationToken'=>$S['__RequestVerificationToken-HEADER'],
-    'ARRAffinity'=>$S['ARRAffinity'],
-    'ASP.NET_SessionId'=>$S['ASP.NET_SessionId']
-  ); 
-  $cookie_txt='';foreach ($cookie as $k=>$v) {$cookie_txt.=urlencode($k).'='.urlencode($v).'; ';};  
-  $ch = curl_init('https://'.$domain.'/Account/Login');
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_txt); 
-  curl_setopt($ch, CURLOPT_COOKIE,$cookie_txt);  
-  curl_setopt($ch, CURLOPT_HEADER, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-  $result = curl_exec($ch); 
-  $info=curl_getinfo($ch);
-  curl_close($ch);
-  preg_match('/.AspNet.ApplicationCookie=([a-zA-Z0-9_-]*);/', $result, $matches, PREG_OFFSET_CAPTURE);
-  if (isset($matches[1][0])){$S['.AspNet.ApplicationCookie']=$matches[1][0];};
-  
+  if ($success){
+    $post=array(
+      '__RequestVerificationToken'=>$FRVT,
+      'Email'=>$Email,
+      'Password'=>$Password,
+      'RememberMe'=>'false'
+    );  
+    $ch = curl_init('https://'.$domain.'/Account/Login');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, txt_p($post)); 
+    curl_setopt($ch, CURLOPT_COOKIE, txt_c($cookie));  
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    $result = curl_exec($ch); 
+    curl_close($ch);
+    preg_match('/.AspNet.ApplicationCookie=([a-zA-Z0-9_-]*);/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){$cookie['.AspNet.ApplicationCookie']=$m[1][0];}else{$success=false; $log[]='Не знайдено Cookie .AspNet.ApplicationCookie';};
+    preg_match('/.AspNet.TwoFactorCookie=([a-zA-Z0-9_-]*);/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){$cookie['.AspNet.TwoFactorCookie']=$m[1][0];}else{$log[]='Не знайдено Cookie .AspNet.TwoFactorCookie';};
+    preg_match('/..AspNet.ExternalCookie=([a-zA-Z0-9_-]*);/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){$cookie['.AspNet.ExternalCookie']=$m[1][0];}else{$log[]='Не знайдено Cookie .AspNet.ExternalCookie';};
+    if (strpos($result, 'Location: /')===false){
+      $log[]='Авторизація невдала';
+      $success=false; 
+    };
+  }; 
+   
   //3. отримати перелік лічильників
-  $cookie=array(
-    '__RequestVerificationToken'=>$S['__RequestVerificationToken-HEADER'],
-    'ARRAffinity'=>$S['ARRAffinity'],
-    'ASP.NET_SessionId'=>$S['ASP.NET_SessionId'],
-    '.AspNet.ApplicationCookie'=>$S['.AspNet.ApplicationCookie'],
-    '.AspNet.TwoFactorCookie'=>$S['.AspNet.TwoFactorCookie'],
-    '.AspNet.ExternalCookie'=>$S['.AspNet.ExternalCookie'],
-  ); 
-  $cookie_txt='';foreach ($cookie as $k=>$v) {$cookie_txt.=urlencode($k).'='.urlencode($v).'; ';};  
-  $ch = curl_init('https://'.$domain.'/consumers');
-  curl_setopt($ch, CURLOPT_HEADER, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-  curl_setopt($ch, CURLOPT_COOKIE,$cookie_txt);  
-  $result = curl_exec($ch); 
-  $info=curl_getinfo($ch);
-  curl_close($ch);
-  preg_match_all('/\/consumers\/([0-9]*)\/info/', $result, $matches, PREG_OFFSET_CAPTURE);
-  foreach ($matches[1] as $k=>$v) {
-    if (!isset($S['Consumer'][$v[0]])){
-      $S['Consumer'][$v[0]]='';  
+  if ($success){  
+    $ch = curl_init('https://'.$domain.'/consumers');
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_COOKIE, txt_c($cookie));  
+    $result = curl_exec($ch);
+    curl_close($ch);
+    preg_match_all('/\/consumers\/([0-9]*)\/info/', $result, $m, PREG_OFFSET_CAPTURE);
+    foreach ($m[1] as $k=>$v) {
+      if (!isset($Consumers[$v[0]])){
+        $Consumers[$v[0]]=array(
+          'cname'=>'',
+          'rr'=>'',
+          'name'=>'',
+          'meters'=>array()
+        );   
+        preg_match('/'.$v[0].'\/info"><strong>(.*)<\/strong>/', $result, $m2, PREG_OFFSET_CAPTURE);
+        if (isset($m2[1][0])){$Consumers[$v[0]]['cname']=$m2[1][0];};
+        preg_match('/'.$v[0].'(.*)<dd>(\d{10}), ([\S ]*)<\/dd>(.*)'.$v[0].'\/forpay/s', $result, $m2, PREG_OFFSET_CAPTURE);
+        if (isset($m2[2][0])){$Consumers[$v[0]]['rr']=$m2[2][0];};
+        if (isset($m2[3][0])){$Consumers[$v[0]]['name']=$m2[3][0];};   
+        $log[]='Знайдено акаунт ID '.$v[0].': '.$Consumers[$v[0]]['rr'].', '.$Consumers[$v[0]]['name'];    
+      };
+    };
+    $success=false;
+    if (isset($Consumers[$ConsumerId])){
+      $log[]='Знайдено ID користувача: '.$ConsumerId;
+      $success=true;   
+    }; 
+    foreach ($Consumers as $k=>$v) {
+      if ($v['rr']==$ConsumerId) {
+        $success=true;
+        $log[]='Знайдено особовий рахунок: '.$ConsumerId.', замінено на ID '.$k;
+        $ConsumerId=$k;
+      };   
+    };
+    if (!$success)  $log[]='Не знайдено: '.$ConsumerId; 
+  };
+  
+  //4. отримати CRF токен для подачі показника  
+  if ($success){ 
+    $ch = curl_init('https://'.$domain.'/consumers/'.$ConsumerId.'/indexes/add');
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_COOKIE,txt_c($cookie));  
+    $result = curl_exec($ch);
+    curl_close($ch);
+    preg_match('/id="reportForm" method="post"><input name="__RequestVerificationToken" type="hidden" value="([a-zA-Z0-9_-]*)" \/>/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){$FRVT=$m[1][0];}else{$success=false; $log[]='Не знайдено у формі __RequestVerificationToken';}; 
+    preg_match('/<th class="text-center" colspan="5">(.*)<\/th>/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){$log[]='Лічильник: '.$m[1][0];};
+    preg_match('/<td class="text-center">([\d.]*)<\/td>/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){$log[]='Дата показника: '.$m[1][0];};
+    preg_match('/<td class="text-right">([\d.]*)<\/td>/', $result, $m, PREG_OFFSET_CAPTURE);
+    if (isset($m[1][0])){
+      $log[]='Попередній показник: '.$m[1][0];
+      if (($CurrentValue-$m[1][0])>500){
+        $success=false;
+        $log[]='Показник новий більше 500 кВт, вихід';
+      }; 
     };
   };
   
-  //4. отримати CRF токен для подачі показника 
-  $cookie=array(
-    '__RequestVerificationToken'=>$S['__RequestVerificationToken-HEADER'],
-    'ARRAffinity'=>$S['ARRAffinity'],
-    'ASP.NET_SessionId'=>$S['ASP.NET_SessionId'],
-    '.AspNet.ApplicationCookie'=>$S['.AspNet.ApplicationCookie'],
-    '.AspNet.TwoFactorCookie'=>$S['.AspNet.TwoFactorCookie'],
-    '.AspNet.ExternalCookie'=>$S['.AspNet.ExternalCookie'],
-  ); 
-  $cookie_txt='';foreach ($cookie as $k=>$v) {$cookie_txt.=urlencode($k).'='.urlencode($v).'; ';};  
-  $ch = curl_init('https://'.$domain.'/consumers/'.$ConsumerId.'/indexes/add');
-  curl_setopt($ch, CURLOPT_HEADER, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-  curl_setopt($ch, CURLOPT_COOKIE,$cookie_txt);  
-  $result = curl_exec($ch); 
-  $info=curl_getinfo($ch);
-  curl_close($ch);
-  preg_match('/__RequestVerificationToken" type="hidden" value="([a-zA-Z0-9_-]*)" \/>/', $result, $matches, PREG_OFFSET_CAPTURE);
-  if (isset($matches[1][0])){$S['__RequestVerificationToken-FORM']=$matches[1][0];}; 
-  
   //5. відпраивти показник
-  $post=array(
-    '__RequestVerificationToken'=>$S['__RequestVerificationToken-FORM'],
-    'ConsumerId'=>$ConsumerId,
-    'ConsumerType'=>'Physical',
-    'CounterMeterages[0].Id'=>'0',
-    'CounterMeterages[0].TimeZoneId'=>'1',
-    'CounterMeterages[0].Quadrant'=>'WithoutQuadrant',
-    'CounterMeterages[0].Counter.UsageCalculationMethodId'=>'57257',
-    'CounterMeterages[0].CurrentValue'=>$CurrentValue
-  );
-  $post_txt='';foreach ($post as $k=>$v) {$post_txt.=(empty($post_txt)?'':'&').urlencode($k).'='.urlencode($v);};
-  $cookie=array(
-    '__RequestVerificationToken'=>$S['__RequestVerificationToken-HEADER'],
-    'ARRAffinity'=>$S['ARRAffinity'],
-    'ASP.NET_SessionId'=>$S['ASP.NET_SessionId'],
-    '.AspNet.ApplicationCookie'=>$S['.AspNet.ApplicationCookie'],
-    '.AspNet.TwoFactorCookie'=>$S['.AspNet.TwoFactorCookie'],
-    '.AspNet.ExternalCookie'=>$S['.AspNet.ExternalCookie'],
-  );  
-  $cookie_txt='';foreach ($cookie as $k=>$v) {$cookie_txt.=urlencode($k).'='.urlencode($v).'; ';};
-  $ch = curl_init('https://'.$domain.'/consumers/'.$ConsumerId.'/indexes/add');
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $post_txt);  
-  curl_setopt($ch, CURLOPT_COOKIE,$cookie_txt);  
-  curl_setopt($ch, CURLOPT_HEADER, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); ;                                                                      
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($post_txt)));                                                                                                                                                                                                                
-  $result = curl_exec($ch); 
-  $info=curl_getinfo($ch);
-  curl_close($ch);
-  //Location: /consumers/*****/indexes
-  
-  //var_dump($S); 
+  if ($success){ 
+    $post=array(
+      '__RequestVerificationToken'=>$FRVT,
+      'ConsumerId'=>$ConsumerId,
+      'ConsumerType'=>'Physical',
+      'CounterMeterages[0].Id'=>'0',
+      'CounterMeterages[0].TimeZoneId'=>'1',
+      'CounterMeterages[0].Quadrant'=>'WithoutQuadrant',
+      'CounterMeterages[0].Counter.UsageCalculationMethodId'=>'57257',
+      'CounterMeterages[0].CurrentValue'=>$CurrentValue
+    ); 
+    $ch = curl_init('https://'.$domain.'/consumers/'.$ConsumerId.'/indexes/add');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, txt_p($post));  
+    curl_setopt($ch, CURLOPT_COOKIE, txt_c($cookie));  
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); ;                                                                                                                                                                                                                                                                                
+    $result = curl_exec($ch); 
+    curl_close($ch);
+    $log[]='Новий показник: '.$CurrentValue;
+    if (strpos($result, 'Location: /consumers/')){
+      $log[]='Дані відправлено';
+    };
+    if (strpos($result, '500 Internal Server Error')){
+      $log[]='Fail add: 500 error';
+      $success= false; 
+    };
+  };
+
+  //6. Отримати результат внесення
+  if ($success){ 
+    $ch = curl_init('https://'.$domain.'/consumers/'.$ConsumerId.'/indexes');
+    curl_setopt($ch, CURLOPT_COOKIE, txt_c($cookie)); 
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    $result = curl_exec($ch); 
+    $info=curl_getinfo($ch);
+    curl_close($ch);  
+    if (strpos($result, 'Покази відправлено в дата центр.')===false){
+      $log[]='Не відправлено показник';
+      $success= false; 
+    }else{
+      $log[]='Покази відправлено в дата центр';        
+    };
+  };
+
+  if ($success & !$debug){
+    return true;
+  }else{
+    return $log;
+  };  
 };
 
 var_dump(loe_webchat(1800123456,49000));
 var_dump(loe_webchat(1800123456,'49000/1455/4445'));
 
-EnergySuite(12345,11111,'user@gmail.com','12345678');
+var_dump(EnergySuite(1800123456,49000,'user@gmail.com','12345678'));
 
 ?>
